ffe4836972f98f8d94991654a3a2a185a0a20280
Orianne <orianne.essienth@etu.unicaen.fr>
2023-11-06 14:20:54 +0100

    corrections sql

--- listeRecettes.php
+++ listeRecettes.php
@@ -37,17 +37,17 @@
         else if (isset($_POST['dessert'])){
             $sqlQuery = 'SELECT distinct REC_ID, REC_IMAGE, CAT_INTITULE, REC_RESUME, REC_TITRE 
             FROM RECETTE,CATEGORIE Where RECETTE.CAT_ID in( Select CAT_ID From CATEGORIE where CAT_INTITULE = "dessert" )
-             and RECETTE.CAT_ID= CATEGORIE.CAT_ID  ; ';
+             and RECETTE.CAT_ID= CATEGORIE.CAT_ID';
             }
 
         else if (isset($_POST['titre'])){
-            $sqlQuery = 'SELECT * FROM RECETTE,CATEGORIE Where REC_TITRE like "%'.$_POST['titre'].'%"';
+            $sqlQuery = 'SELECT DISTINCT(REC_ID),REC_TITRE,REC_IMAGE,CAT_INTITULE,REC_RESUME FROM RECETTE,CATEGORIE Where REC_TITRE like "%'.$_POST['titre'].'%"and RECETTE.CAT_ID= CATEGORIE.CAT_ID';
             }
 
         else if (isset($_POST['ingredient'])){
             $sqlQuery = 'SELECT DISTINCT(REC_ID),REC_TITRE,REC_IMAGE,CAT_INTITULE,REC_RESUME 
             FROM RECETTE,INGREDIENT,CATEGORIE WHERE lower(ING_INTITULE) 
-            like "%'.$_POST['ingredient'].'%" and REC_CONTENU like "%'.$_POST['ingredient'].'%" and RECETTE.CAT_ID= CATEGORIE.CAT_ID;';
+            like "%'.$_POST['ingredient'].'%" and lower(REC_CONTENU) like "%'.$_POST['ingredient'].'%" and RECETTE.CAT_ID= CATEGORIE.CAT_ID';
             //"%'.$_POST['ingredient'].'%" or REC_TITRE like "%'.$_POST['ingredient'].'%"';
             }
 
