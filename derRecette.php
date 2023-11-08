e09ef9d8c8927b0a87a9188460c23bb1d96629a9
hugo <hugo.lagauzere@etu.unicaen.fr>
2023-11-08 15:12:11 +0100

    :fire: deleting USELESS files

--- derRecette.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php
-    function getPosts() {
-        require_once 'connexion.php';
-        $req = $bdd->query('SELECT * FROM RECETTE creation_date  order by rec_date_creation desc LIMIT 0,3');
-        return $req;
-    }
-?>
